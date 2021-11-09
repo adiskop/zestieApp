6da4d95bcbc438cbd7a185c181a42293c18dddd7
Adi Skop <adiskop@gmail.com>
2021-11-09 12:47:22 +0100

    Created file

---
+++ zestieApp/zestieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zestieApp
+//
+//  Created by Adi Skop on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
